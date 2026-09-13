6d455bf9661f946bff356ca2747d4e8fd55d8ab5
Devin Mateo <200mshinobi@gmail.com>
2026-09-13 16:48:32 -0400

    changed price id

--- api/configs/pricing.py
+++ api/configs/pricing.py
@@ -17,7 +17,7 @@ else:
     print("[Stripe] WARNING: STRIPE_API_KEY is not set.")
 
 plan_price_id = {
-    "price": "price_1UCmbhHvFcnfvp6wZbt32noQ",
+    "Foundation": "price_1UCmbhHvFcnfvp6wZbt32noQ",
 }
 
 
